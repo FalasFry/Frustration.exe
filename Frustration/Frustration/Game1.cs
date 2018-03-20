a74a0cde9eb90c5c9680b88d9f66ca5d1d175243
BuildTools <unconfigured@null.spigotmc.org>
2018-03-20 14:32:12 +0100

    Game1 changes

--- Frustration/Frustration/Game1.cs
+++ Frustration/Frustration/Game1.cs
@@ -18,7 +18,7 @@ namespace Frustration
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
+        /// <Initialize>
         /// Allows the game to perform any initialization it needs to before starting to run.
         /// This is where it can query for any required services and load any non-graphic
         /// related content.  Calling base.Initialize will enumerate through any components
@@ -29,9 +29,10 @@ namespace Frustration
             // TODO: Add your initialization logic here
 
             base.Initialize();
+            IsMouseVisible = true;
         }
 
-        /// <summary>
+        /// <LoadContent>
         /// LoadContent will be called once per game and is the place to load
         /// all of your content.
         /// </summary>
@@ -43,7 +44,7 @@ namespace Frustration
             // TODO: use this.Content to load your game content here
         }
 
-        /// <summary>
+        /// <Unload>
         /// UnloadContent will be called once per game and is the place to unload
         /// game-specific content.
         /// </summary>
@@ -52,7 +53,7 @@ namespace Frustration
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
+        /// <Update>
         /// Allows the game to run logic such as updating the world,
         /// checking for collisions, gathering input, and playing audio.
         /// </summary>
@@ -67,13 +68,13 @@ namespace Frustration
             base.Update(gameTime);
         }
 
-        /// <summary>
+        /// <Draw>
         /// This is called when the game should draw itself.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.White);
 
             // TODO: Add your drawing code here
 
