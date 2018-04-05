608f3b851e76a7c80781674720775a6667523854
Alvin Andersson Eriksson <alvin.anderssonerik@learnet.se>
2018-04-05 11:06:45 +0200

    hello

---
+++ Frustration/Frustration/Bullet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Frustration
+{
+    public class Bullet
+    {
+        public float speed;
+        public Vector2 dir;
+        public Texture2D texture;
+        public Rectangle rectangle;
+        
+        public Bullet(float Speed,Vector2 Dir,Texture2D Texture, Rectangle Rectangle)
+        {
+            speed = Speed;
+            dir = Dir;
+            texture = Texture;
+            rectangle = Rectangle;
+        }
+        public void Move()
+        {
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture,rectangle,Color.Black);
+        }
+    }
+}
