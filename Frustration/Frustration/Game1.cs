7638513edb72186d9068547c3678615697a9d146
BuildTools <unconfigured@null.spigotmc.org>
2018-04-24 10:24:05 +0200

    dsa

--- Frustration/Frustration/Game1.cs
+++ Frustration/Frustration/Game1.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 
 
 namespace Frustration
@@ -69,7 +70,6 @@ namespace Frustration
 
         protected override void Update(GameTime gameTime)
         {
-
             if (stateStack.Peek().Update(gameTime) == false)
             {
                 stateStack.Pop();
